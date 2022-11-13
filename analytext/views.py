0d9c4c027f308815d3ffb020348b058680841ff7
Vishal Prakapati <vishal.kumar190601@gmail.com>
2022-11-13 12:20:07 +0530

    New update

--- analytext/views.py
+++ analytext/views.py
@@ -4,5 +4,21 @@ from django.shortcuts import render
 def index(req):
     return render(req,"index.html")
 
-def about(req):
-    return HttpResponse("About")    
+def analyze(req):
+    text = req.GET.get("text","Default")
+    removePunc = req.GET.get("removepunc","off")
+    capitalize = req.GET.get("capitalize","off")
+    analyzed = ""
+    
+    if removePunc == "on":
+        validPunch = '''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+        for char in text:
+            if char not in validPunch:
+                analyzed += char 
+
+    # Capitalize the text
+    elif capitalize == "on":
+        analyzed = text.capitalize()
+
+    data = {"text":analyzed}
+    return render(req,"analyze.html",data)
