0f9349b2a23ef3ad17dcb8c37f4578b5cb2bcceb
Vishal Prakapati <vishal.kumar190601@gmail.com>
2022-11-16 15:59:15 +0530

    All Most Finished

--- analytext/views.py
+++ analytext/views.py
@@ -5,35 +5,52 @@ def index(req):
     return render(req,"index.html")
 
 def analyze(req):
-    text = req.GET.get("text","Default")
-    removePunc = req.GET.get("removepunc","off")
-    capitalize = req.GET.get("capitalize","off")
-    uppercase = req.GET.get('uppercase','off')
-    lowercase = req.GET.get('lowercase')
-    newLineRemover = req.GET.get('newlineremover')
+    text = req.POST.get("text","Default")
+    removePunc = req.POST.get("removepunc","off")
+    capitalize = req.POST.get("capitalize","off")
+    uppercase = req.POST.get('uppercase','off')
+    lowercase = req.GET.get('lowercase','off')
+    newLineRemover = req.POST.get('newlineremover','off')
+    spaceremover = req.POST.get("spaceremover",'off')
     analyzed = ""
     
     # Remveve Punchuation
     if removePunc == "on":
-        validPunch = '''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+        validPunch = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
         for char in text:
             if char not in validPunch:
                 analyzed += char 
+                text = analyzed
 
     # Capitalize the text
-    elif capitalize == "on":
+    if capitalize == "on":
         analyzed = text.capitalize()
+        text = analyzed
 
     # Upcase Text
-    elif uppercase == "on":
+    if uppercase == "on":
         analyzed = text.upper()
+        text = analyzed
+
 
     # Lowercase Text
-    elif lowercase == "on":
+    if lowercase == "on":
         analyzed = text.lower()
+        text = analyzed
 
-    elif newLineRemover == "on":
-        pass
+    # New line Remover
+    if newLineRemover == "on":
+        for char in text:
+            if char != "\n" and char != "\r":
+                analyzed += char
+        text = analyzed        
+
+    # Space Remover
+    if spaceremover == "on":
+        for index,char in enumerate(text):
+            if not (text[index] == " " and text[index + 1] == " "):
+                analyzed += char       
+        text = analyzed        
     
-    data = {"text":analyzed}
+    data = {"text":text}
     return render(req,"analyze.html",data)
